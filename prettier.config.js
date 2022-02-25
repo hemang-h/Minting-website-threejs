282be9694587c0558e5b08d3604c9cf8d894e94e
hemang-h <hemangcertificate@gmail.com>
2022-02-26 04:26:29 +0530

    Config

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+    arrowParens: 'avoid',
+    singleQuote: true,
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    semi: false,
+    tabwidth: 2,
+    trailingComa: 'none'
+}
+
+module.exports = options 
