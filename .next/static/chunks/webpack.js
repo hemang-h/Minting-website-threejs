0df9f122614e6daf6842def915d74d87867ebd13
hemang-h <hemangcertificate@gmail.com>
2022-02-27 01:49:43 +0530

    Modification

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "19adb86056eca122"; }
+/******/ 		__webpack_require__.h = function() { return "1ceb9a5a066631da"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ components/layouts/main.js
@@ -0,0 +1,15 @@
+ import Head from 'next/head'
+ import {Box, container} from '@chakra-ui/react'
+
+ const Main = ({children, router}) => {
+     return(
+         <Box as="main" pb={8}>
+             <Head>
+                 <meta name="viewport" content="width-device-width, intial-scale=1" />
+                 <meta name="description" content="TOkenMinds Minting" />
+             </Head>
+
+             <container maxW="container.md" pt={14}></container>
+         </Box>
+     )
+ }
