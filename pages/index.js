719014473e4e368974a61f819561b71a87ca8aa5
hemang-h <hemangcertificate@gmail.com>
2022-02-27 18:10:29 +0530

    Box making

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import {Container, Box, Center} from '@chakra-ui/react'
+
 const page = () => {
-    return <div>Hello</div>
+    return(
+        <Container>
+            <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg="blue"
+      >
+        Who wants a free Airdrop
+      </Box>
+        </Container>
+    )
 }
 
 export default page 
