eac3373b3085810f9226e154636bb312dbf07caa
hemang-h <hemangcertificate@gmail.com>
2022-02-27 20:46:31 +0530

    FIxed Imports

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,56 @@
 import {
-    Container, 
-    Box, 
-    Center,
-    Heading
+    Link,
+  Container,
+  Heading,
+  Box,
+  SimpleGrid,
+  Button,
+  List,
+  ListItem,
+  useColorModeValue,
+  chakra
 } from '@chakra-ui/react'
 
 const page = () => {
     return(
         <Container>
-            <Box
+    <Box
         borderRadius="lg"
         mb={6}
         p={3}
         textAlign="center"
-        bg="blue"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
       >
-        Who wants a free Airdrop
+        Welcome to the a Standard Demo website to mint NFTs
       </Box>
 
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
-            TokenMinds 
+            TokenMinds
           </Heading>
-          <p>Marketing/IDO/Development</p>
+          <p>Full servie Blockchain & NFT Agency (Marketing / Developement)</p>
         </Box>
-     </Box> 
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow='hidden'
+          >
+            
+          </Box>
+        </Box>
+      </Box>
         </Container>
     )
 }
