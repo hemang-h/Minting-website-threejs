fc05fc11e3dedfcf8ed2ee100203c5c885a7c8b6
hemang-h <hemangcertificate@gmail.com>
2022-03-02 01:10:36 +0530

    modification

--- components/navbar.js
+++ components/navbar.js
@@ -72,7 +72,7 @@ const Navbar = props => {
           mt={{ base: 4, md: 0 }}
         >
           <LinkItem href="/works" path={path}>
-            Works
+            Minting 
           </LinkItem>
           <LinkItem href="/posts" path={path}>
             Posts
