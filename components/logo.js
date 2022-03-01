5acc52411db13e82d99c75c287938a4477bc8376
hemang-h <hemangcertificate@gmail.com>
2022-03-02 01:23:10 +0530

    Added font

--- components/logo.js
+++ components/logo.js
@@ -29,7 +29,7 @@ const Logo = () => {
           <Image src={footPrintImg} width={20} height={20} alt="logo" />
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
-            fontFamily='Raleway", sans-serif'
+            fontFamily='M PLUS Rounded 1c", sans-serif'
             fontWeight="bold"
             ml={3}
           >
