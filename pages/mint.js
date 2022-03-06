ef5d9ba8c5eaee1bfbbca47b4750f36a9b14c826
hemang-h <hemangcertificate@gmail.com>
2022-03-06 20:20:17 +0530

    imported web3Modal

--- pages/mint.js
+++ pages/mint.js
@@ -2,10 +2,12 @@ import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
-import ConnectWallet from '../components/Metamask/ConnectWallet' 
+import useWeb3Modal from '../hooks/useWeb3Modal'
+
 
 
 const mint = () => (
+  const { web3provider } = useWeb3Modal() 
   <Layout title="Minting">
     <Container>
       <Heading as="h3" fontSize={20} mb={4}>
